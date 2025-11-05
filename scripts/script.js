479de2f006a8fb4ac5871fc9c7712cb4d6fb6e25
Yahia <nasriyahia42@gmail.com>
2025-11-05 17:56:53 +0100

    reset

--- scripts/script.js
+++ scripts/script.js
@@ -5,20 +5,41 @@
   4/ display it in the result-display
   5/ display the icons of the moves
   6/ add score
-  7/ add a score and save it in document & JSON
-  8/ add auto play feature
+  7/ save the score in document & JSON
+  8/ add a restart button
+  9/ add auto play feature
 
   1/ Done!
   2/ Done!
   3/ Done!
   4/ Done!
+  5/ Done!
+  6/ Done!
+  7/ Done!
 */
-const resultText = document.querySelector('.result');
+const resultDisplay = document.querySelector('.js-result-text');
+const computerSection = document.querySelector('.computer-move');
+const youSection = document.querySelector('.you-move');
+
+const scoreText = document.querySelector('.result');
+
+const roundText = document.querySelector('.js-round');
+
+let round = JSON.parse(localStorage.getItem('round')) || 0;
 
 let score = JSON.parse(localStorage.getItem('score')) || {
   wins: 0,
   losses: 0,
 }
+resultDisplay.innerText = JSON.parse(localStorage.getItem('resultDisplay')) || 'Start Playing';
+
+let playerMove =  JSON.parse(localStorage.getItem('player-move')) || '' ;
+let computerMove = JSON.parse(localStorage.getItem('computer-move')) || '' ;
+
+console.log(computerMove + ' ' + playerMove);
+
+updateUI(playerMove,computerMove);
+
 
 document.querySelector('.js-rock-move').addEventListener('click',() => {
   playGame('rock');
@@ -30,14 +51,19 @@ document.querySelector('.js-scissors-move').addEventListener('click',() => {
   playGame('scissors');
 })
 
+document.querySelector('.js-reset-button').addEventListener('click',() => {
+  reset();
+})
 
 
-const textDisplay = document.querySelector('.js-result-text');
-const computerSection = document.querySelector('.computer-move');
-const youSection = document.querySelector('.you-move');
+
+
 
 function playGame(playerMove) {
-  const computerMove = pickComputerMove();
+  roundText.innerText = `Round: ${round++}`;
+  
+
+  computerMove = pickComputerMove();
   let result;
 
 
@@ -58,11 +84,16 @@ function playGame(playerMove) {
       score.losses++;
     }
   }
-  console.log(score);
-  
-  textDisplay.innerText = `${result} the round!`;
 
-  updateUI(computerMove,playerMove);
+  
+  resultDisplay.innerText = `${result} the round!`;
+
+  localStorage.setItem('score',JSON.stringify(score));
+  localStorage.setItem('player-move',JSON.stringify(playerMove));
+  localStorage.setItem('computer-move',JSON.stringify(computerMove));
+  localStorage.setItem('round',JSON.stringify(round));
+
+  updateUI(playerMove,computerMove);
 }
 
 function pickComputerMove() {
@@ -77,15 +108,37 @@ function pickComputerMove() {
   else if (randomNumber === 3) {
     return 'scissors';
   }
-  else{
-    alert(`${randomNumber}`);
-  }
 }
 
-function updateUI(computerMove,playerMove) {
+function updateUI(playerMove,computerMove) {
+  scoreText.innerText = `${score.wins} : ${score.losses}`;
+  roundText.innerText = `Round: ${round}`;
+
+
+
+  if (computerMove === '' || playerMove === '') {
+    computerSection.innerHTML = '';
+    youSection.innerHTML = '';
+    return;
+  }
+
+
   computerSection.innerHTML = `<img src="../Assets/Icons/${computerMove}-emoji.png" class="move-icon">`;
 
   youSection.innerHTML = `<img src="../Assets/Icons/${playerMove}-emoji.png" class="move-icon">`;
+}
 
-  resultText.innerText = `${score.wins} : ${score.losses}`
+function reset() {
+  score = {
+  wins: 0,
+  losses: 0,
+  }
+
+  computerMove = '';
+  playerMove = '';
+  round = 0;
+
+  resultDisplay.innerText = 'Start Playing';
+
+  updateUI(computerMove,playerMove);
 }
